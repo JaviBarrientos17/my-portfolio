d2fe652d91bb04a80c91a85a700cc263c34cd0c2
JaviBarrientos17 <javierbarrientos2010@gmail.com>
2022-12-26 19:48:44 +0100

    Adding navbar

---
+++ components/_app.js
@@ -0,0 +1,13 @@
+import Navbar from "../components/Navbar";
+import "../styles/globals.css";
+
+function MyApp() {
+  return (
+    <>
+      <Navbar />
+      {/* <Component {...pageProps} /> */}
+    </>
+  );
+}
+
+export default MyApp;
