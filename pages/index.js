b52e15592f2b54fdf6e26715767cba2acdebc918
JaviBarrientos17 <javierbarrientos2010@gmail.com>
2022-12-27 13:54:22 +0100

    Working nextjs components

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
-import Navbar from "../components/Navbar";
+import { NextUIProvider } from '@nextui-org/react';
 
 export default function Home() {
   return (
-    <main className="h-screen w-screen flex flex-col items-center justify-center  bg-white dark:bg-black">
-      <h1 className="text-3xl font-bold text-blue-500 dark:text-red-500">
-        Hello world!
-      </h1>
-    </main>
+    <NextUIProvider>
+      <main className="h-screen w-screen flex flex-col items-center justify-center  bg-white dark:bg-black">
+        <h1 className="text-3xl font-bold text-blue-500 dark:text-red-500">
+          Hello world!
+        </h1>
+      </main>
+    </NextUIProvider>
   );
 }
