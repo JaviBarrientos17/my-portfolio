417192c2ed76c204363d85515dbee6f8096fe1ac
JaviBarrientos17 <javierbarrientos2010@gmail.com>
2022-12-28 09:58:05 +0100

    Reworking navbar with snow js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,13 @@
+import { Fragment } from "react";
 import Navbar from "../components/Navbar";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <Fragment>
       <Navbar />
       <Component {...pageProps} />
-    </>
+    </Fragment>
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
+import { Fragment } from "react";
 import Navbar from "../components/Navbar";
 
 export default function Home() {
   return (
-    <div className="center">
-      <h2>Home Page</h2>
-    </div>
+    <Fragment>
+      <div className="center">
+        <h2>Home Page</h2>
+      </div>
+       {/* TODO MAKE WORK SNOW JS */}
+       <script src="https://app.embed.im/snow.js" defer></script>
+    </Fragment>
   );
 }
