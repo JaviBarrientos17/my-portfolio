c35584ded5aca4da084364fe06851b273520ac08
JaviBarrientos17 <javierbarrientos2010@gmail.com>
2022-12-28 09:35:00 +0100

    Deleted _document.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,20 @@
 import Script from "next/script";
 import Navbar from "../components/Navbar";
-import { NextUIProvider } from "@nextui-org/react";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <NextUIProvider>
-        {/* TODO SNOW JS SCRIPT */}
-        <Script src="https://app.embed.im/snow.js" defer></Script>
-        {/* TODO SEARCH HOW TO CHANGE THE TAB ICON */}
-        <link
-          rel="shortcut icon"
-          href="https://avatars.githubusercontent.com/u/44582104?v=4"
-        />
-        <title>Javi Barrientos Peralta | Portfolio</title>
-        <Navbar />
-        <Component {...pageProps} />
-      </NextUIProvider>
+      {/* TODO SNOW JS SCRIPT */}
+      <Script src="https://app.embed.im/snow.js" defer></Script>
+      {/* TODO SEARCH HOW TO CHANGE THE TAB ICON */}
+      <link
+        rel="shortcut icon"
+        href="https://avatars.githubusercontent.com/u/44582104?v=4"
+      />
+      <title>Javi Barrientos Peralta | Portfolio</title>
+      <Navbar />
+      <Component {...pageProps} />
     </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,9 @@
-import { NextUIProvider } from '@nextui-org/react';
-
 export default function Home() {
   return (
-    <NextUIProvider>
       <main className="h-screen w-screen flex flex-col items-center justify-center  bg-white dark:bg-black">
         <h1 className="text-3xl font-bold text-blue-500 dark:text-red-500">
           Hello world!
         </h1>
       </main>
-    </NextUIProvider>
   );
 }
