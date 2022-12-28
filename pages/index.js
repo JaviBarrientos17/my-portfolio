5331b8dfe0945889aa03e64068765be8412dc9c8
JaviBarrientos17 <javierbarrientos2010@gmail.com>
2022-12-28 21:43:24 +0100

    Added title

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,12 @@
 import { Fragment } from "react";
-import Navbar from "../components/Navbar";
 
 export default function Home() {
   return (
     <Fragment>
+      <title>Javi Barrientos Peralta | Portfolio</title>
       <div className="center">
         <h2>Home Page</h2>
       </div>
-       {/* TODO MAKE WORK SNOW JS */}
        <script src="https://app.embed.im/snow.js" defer></script>
     </Fragment>
   );
