e8636dc279000ada47db742a0763db2a08579dcd
JaviBarrientos17 <javierbarrientos2010@gmail.com>
2023-01-11 11:39:43 +0100

    Uncomment snow.js

--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,8 @@ export default function Home() {
       <div className="center">
         <h2>Home Page</h2>
       </div>
-      {/* TODO COMENT THIS WHEN THERE IS NOT CHRISTMAS */}
-      <script src="https://app.embed.im/snow.js" defer></script>
+      {/* TODO UNCOMMENT THIS WHEN THERE IS CHRISTMAS */}
+      {/* <script src="https://app.embed.im/snow.js" defer></script> */}
     </Fragment>
   );
 }
