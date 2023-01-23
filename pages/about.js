28d525f62349f6f77cf446855d4e1b4c2b85196b
JaviBarrientos17 <javierbarrientos2010@gmail.com>
2023-01-23 17:52:40 +0100

    Working download CV

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,29 @@
 import React, { Fragment } from "react";
+import { Button, ButtonGroup, HStack } from '@chakra-ui/react'
 
 const About = () => {
   return (
     <Fragment>
       <title>Javi Barrientos Peralta | Portfolio</title>
       <div className="center">
-        <h2>About Page</h2>
+        <h2></h2>
+        <a
+          name="downloadCVBtn"
+          id="downloadCVBtn"
+          class="btn btn-primary"
+          href="#"
+          role="button"
+        >
+          Download CV
+        </a>
+        {/* <HStack>
+          <Button colorScheme="facebook" leftIcon={<FaFacebook />}>
+            Facebook
+          </Button>
+          <Button colorScheme="twitter" leftIcon={<FaTwitter />}>
+            Twitter
+          </Button>
+        </HStack> */}
       </div>
     </Fragment>
   );
