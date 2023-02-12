1eef2a5c6ba7988a69e069a34ca6bf936112c3b4
JaviBarrientos17 <javierbarrientos2010@gmail.com>
2023-02-12 19:18:31 +0100

    Added card ReactDex

--- pages/projects.js
+++ pages/projects.js
@@ -1,13 +1,39 @@
 import React, { Fragment } from "react";
+import ThemeToggle from "../components/ThemeToggle";
 
 const Projects = () => {
   return (
     <Fragment>
       <title>Javi Barrientos Peralta | Portfolio</title>
-      <div className="center">
-        <h2>Projects Page</h2>
-        {/* TODO ADD CARDS OR SOMETHING SIMILAR ABOUT MY MAIN PROJECTS THAT I'VE DONE */}
-      </div>
+      {/* <div className="gridDiv">
+        <div className="card">
+          <img src="https://pkreactdex.netlify.app/static/media/ReactDex.07b1fe57db71ff3dc1cb.png" />
+          <section>
+            <h4>Pokedex project using the PokeApi and React</h4>
+            <button className="primary-button">Link</button>
+          </section>
+        </div>
+      </div> */}
+      <section className="gridSection">
+        <div class="container">
+          <main class="grid">
+            <article>
+              <img
+                src="https://i.imgur.com/bPZegib.jpg"
+                alt="ReactDex homepage"
+              />
+              <div class="text">
+                {/* <p className="cardTitle">ReactDex</p> */}
+                <img src="https://tinyurl.com/5ysvxbr5" alt="ReactDex logo" />
+                <p>Pokedex project using the PokeApi and React</p>
+                <a href="https://pkreactdex.netlify.app/"><button className="primary-button">Link</button></a>
+                {/* // TODO ADD SPACE BETWEEN THE TWO BTNS */}
+                <a href="https://github.com/JaviBarrientos17/ReactDex"><button className="primary-button">Link</button></a>
+              </div>
+            </article>
+          </main>
+        </div>
+      </section>
     </Fragment>
   );
 };
