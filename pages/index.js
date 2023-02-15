b02a4cd4541e8c39741652db1ff3da5ca6b38572
elpepesito3345 <elpepesito3345@gmail.com>
2023-02-15 10:05:45 +0100

    Make work TabNotification.js

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,22 @@
 import { Fragment } from "react";
-import { ChakraProvider } from "@chakra-ui/react";
 import Head from "next/head";
-import ThemeToggle from "../components/ThemeToggle";
+import Script from "next/script";
 
 export default function Home() {
   return (
-      <Fragment>
-        <Head>
-          <title>Javi Barrientos Peralta | Portfolio</title>
-          <meta
-            name="description"
-            content="Javi Barrientos Peralta | Portfolio"
-          />
-          {/* <link rel="icon" href="/favicon.ico" /> */}
-        </Head>
-        {/* TODO UNCOMMENT THIS WHEN THERE IS CHRISTMAS */}
-        {/* <script src="https://app.embed.im/snow.js" defer></script> */}
-      </Fragment>
+    <Fragment>
+      {/* TODO MAKE WORK THIS FILE LOCATION */}
+      <Script src="assets/js/TabNotification.js" defer />
+      <Head>
+        <title>Javi Barrientos Peralta | Portfolio</title>
+        <meta
+          name="description"
+          content="Javi Barrientos Peralta | Portfolio"
+        />
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+      </Head>
+      {/* TODO UNCOMMENT THIS WHEN THERE IS CHRISTMAS */}
+      {/* <script src="https://app.embed.im/snow.js" defer></S> */}
+    </Fragment>
   );
 }
