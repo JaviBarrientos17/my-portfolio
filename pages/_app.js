497d16699b30769e45fa148e6c37809f67c58b1e
elpepesito3345 <elpepesito3345@gmail.com>
2023-02-16 10:50:42 +0100

    Added Head and custom js script

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,17 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { Fragment } from "react";
 import Navbar from "../components/Navbar";
+import Script from "next/script";
+import Head from "next/head";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
       <Fragment>
+        <Head>
+          <Script type="text/javascript" src="../public/static/TabNotification.js" defer/>
+        </Head>
         <Navbar />
         <Component {...pageProps} />
       </Fragment>
