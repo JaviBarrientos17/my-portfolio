6ca815f3e421db8c4035f304fc8b774405f704b1
JaviBarrientos17 <javierbarrientos2010@gmail.com>
2023-02-17 17:28:23 +0100

    Commented weather-app card

--- pages/projects.js
+++ pages/projects.js
@@ -33,21 +33,20 @@ const Projects = () => {
                 src="https://i.imgur.com/bPZegib.jpg"
                 alt="ReactDex homepage"
               />
-              <div className="card">
+              {/* <div className="card">
                 <img src="https://tinyurl.com/5cjy2a4x" alt="Forecast Checker logo" />
                 <p>Weather app, built with NextJS and the <a href="https://openweathermap.org/api">Weather API from OpenWeather</a></p>
                 <div className="button-container">
                   <a href="https://forecast-checker.netlify.app/">
                     <button className="primary-button linkBtns">Link</button>
                   </a>
-                  {/* TODO CHANGE GIT REPOSITORY LINK */}
                   <a href="https://github.com/JaviBarrientos17/ReactDex">
                     <button className="primary-button linkBtns">
                       <AiFillGithub size={25} />
                     </button>
                   </a>
                 </div>
-              </div>
+              </div> */}
             </article>
             <article>
               <img
